9eb95f68e859d47eb97b7d1b28360910bfc997b0
Bastien Nocera <hadess@hadess.net>
2014-10-21 19:49:12 +0200

    lib: Add deprecation guards around gdk_pixbuf_new_from_inline()

--- gdk-pixbuf/gdk-pixbuf-core.h
+++ gdk-pixbuf/gdk-pixbuf-core.h
@@ -308,11 +308,15 @@ GdkPixbuf *gdk_pixbuf_new_from_bytes (GBytes *data,
 				      int rowstride);
  
 GdkPixbuf *gdk_pixbuf_new_from_xpm_data (const char **data);
+
+#ifndef GDK_PIXBUF_DISABLE_DEPRECATED
+G_DEPRECATED
 GdkPixbuf* gdk_pixbuf_new_from_inline	(gint          data_length,
 					 const guint8 *data,
 					 gboolean      copy_pixels,
 					 GError      **error);
-       
+#endif
+
 /* Mutations */
 void       gdk_pixbuf_fill              (GdkPixbuf    *pixbuf,
                                          guint32       pixel);
