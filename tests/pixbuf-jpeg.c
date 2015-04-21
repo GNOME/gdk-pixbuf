e8d0ced40555bd4f99508af3e3d214db502933a9
Bastien Nocera <hadess@hadess.net>
2015-04-21 13:30:26 +0200

    tests: Add test for wrongly typed orientation EXIF tag
    Note that the rotation, once applied, should have the test photo
    looking sideways (vertical) and not horizontal as you'd expect.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=725582

--- tests/pixbuf-jpeg.c
+++ tests/pixbuf-jpeg.c
@@ -50,12 +50,42 @@ test_inverted_cmyk_jpeg (void)
   g_object_unref (ref2);
 }
 
+static void
+test_type9_rotation_exif_tag (void)
+{
+  GError *error = NULL;
+  GdkPixbuf *ref, *ref1, *ref2;
+  gboolean ret;
+
+  if (!format_supported ("jpeg") || !format_supported ("png"))
+    {
+      g_test_skip ("format not supported");
+      return;
+    }
+
+  ref = gdk_pixbuf_new_from_file (g_test_get_filename (G_TEST_DIST, "bug725582-testrotate.jpg", NULL), &error);
+  g_assert_no_error (error);
+  ref1 = gdk_pixbuf_apply_embedded_orientation (ref);
+
+  ref2 = gdk_pixbuf_new_from_file (g_test_get_filename (G_TEST_DIST, "bug725582-testrotate.png", NULL), &error);
+  g_assert_no_error (error);
+
+  ret = pixdata_equal (ref1, ref2, &error);
+  g_assert_no_error (error);
+  g_assert (ret);
+
+  g_assert_cmpstr (gdk_pixbuf_get_option (ref, "orientation"), ==, "6");
+
+  g_object_unref (ref);
+}
+
 int
 main (int argc, char **argv)
 {
   g_test_init (&argc, &argv, NULL);
 
   g_test_add_func ("/pixbuf/jpeg/inverted_cmyk_jpeg", test_inverted_cmyk_jpeg);
+  g_test_add_func ("/pixbuf/jpeg/type9_rotation_exif_tag", test_type9_rotation_exif_tag);
 
   return g_test_run ();
 }
