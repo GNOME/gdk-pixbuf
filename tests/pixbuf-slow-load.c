f1837aaf08cac2b3d0635452a00f318131fddc55
Benjamin Otte <otte@redhat.com>
2015-10-04 02:20:29 +0200

    test-common: If image data differs, output first coords that fail

--- tests/pixbuf-slow-load.c
+++ tests/pixbuf-slow-load.c
@@ -33,7 +33,26 @@ loader_size_prepared (GdkPixbufLoader  *loader,
 {
   g_assert (*pixbuf == NULL);
 
-  *pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, TRUE, 8, w, h);
+  *pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, w, h);
+  g_assert (*pixbuf != NULL);
+}
+
+static void
+loader_area_prepared (GdkPixbufLoader  *loader,
+                      int               w,
+                      int               h,
+                      GdkPixbuf       **pixbuf)
+{
+  g_assert (*pixbuf != NULL);
+
+  if (gdk_pixbuf_get_has_alpha (gdk_pixbuf_loader_get_pixbuf (loader)))
+    {
+      GdkPixbuf *alpha = gdk_pixbuf_add_alpha (*pixbuf, FALSE, 0, 0, 0);
+
+      g_object_unref (*pixbuf);
+      *pixbuf = alpha;
+    }
+
   g_assert (*pixbuf != NULL);
 }
 
@@ -52,56 +71,71 @@ loader_area_updated (GdkPixbufLoader  *loader,
                         x, y);
 }
 
+static char *
+make_ref_filename (const char *filename)
+{
+  return g_strconcat (filename, ".ref.png", NULL);
+}
+
+static gboolean
+is_ref_filename (const char *filename)
+{
+  return g_str_has_suffix (filename, ".ref.png");
+}
+
 static void
-test_slow_load (gconstpointer file)
+test_reftest_success (gconstpointer file)
 {
   GdkPixbufLoader *loader;
   GdkPixbuf *reference, *loaded = NULL;
   GError *error = NULL;
   const char *filename;
+  char *ref_filename;
   guchar *contents;
   gsize i, contents_length;
+  gboolean success;
 
   filename = file;
-  reference = gdk_pixbuf_new_from_file (filename, &error);
+  if (is_ref_filename (filename))
+    return;
+
+  ref_filename = make_ref_filename (filename);
+  reference = gdk_pixbuf_new_from_file (ref_filename, &error);
   g_assert_no_error (error);
   g_assert (reference != NULL);
-  if (!gdk_pixbuf_get_has_alpha (reference))
-    {
-      GdkPixbuf *tmp = gdk_pixbuf_add_alpha (reference, FALSE, 0, 0, 0);
-      g_object_unref (reference);
-      reference = tmp;
-    }
 
-  loader = gdk_pixbuf_loader_new_with_mime_type ("image/x-tga", &error);
-  g_assert_no_error (error);
+  loader = gdk_pixbuf_loader_new ();
   g_assert (loader != NULL);
   g_signal_connect (loader, "size-prepared", G_CALLBACK (loader_size_prepared), &loaded);
+  g_signal_connect (loader, "area-prepared", G_CALLBACK (loader_area_prepared), &loaded);
   g_signal_connect (loader, "area-updated", G_CALLBACK (loader_area_updated), &loaded);
 
-  if (!g_file_get_contents (filename, (gchar **) &contents, &contents_length, &error))
-    g_assert_not_reached ();
+  success = g_file_get_contents (filename, (gchar **) &contents, &contents_length, &error);
   g_assert_no_error (error);
+  g_assert (success);
 
   for (i = 0; i < contents_length; i++)
     {
-      if (!gdk_pixbuf_loader_write (loader, &contents[i], 1, &error))
-        g_assert_not_reached();
+      success = gdk_pixbuf_loader_write (loader, &contents[i], 1, &error);
       g_assert_no_error (error);
+      g_assert (success);
     }
   
-  if (!gdk_pixbuf_loader_close (loader, &error))
-    g_assert_not_reached ();
+  success = gdk_pixbuf_loader_close (loader, &error);
   g_assert_no_error (error);
+  g_assert (success);
+
   g_assert (loaded != NULL);
 
-  g_assert (pixdata_equal (reference, loaded, &error));
+  success = pixdata_equal (reference, loaded, &error);
   g_assert_no_error (error);
+  g_assert (success);
 
   g_free (contents);
   g_object_unref (loaded);
   g_object_unref (loader);
   g_object_unref (reference);
+  g_free (ref_filename);
 }
 
 int
@@ -112,7 +146,7 @@ main (int argc, char **argv)
   g_test_init (&argc, &argv, NULL);
 
   tga_test_images = g_build_filename (g_test_get_dir (G_TEST_DIST), "test-images/tga", NULL);
-  add_test_for_all_images ("/pixbuf/slow-load", tga_test_images, test_slow_load);
+  add_test_for_all_images ("/pixbuf/reftest/success", tga_test_images, test_reftest_success);
   g_free (tga_test_images);
 
   return g_test_run ();
