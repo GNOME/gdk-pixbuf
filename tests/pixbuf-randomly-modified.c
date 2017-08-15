dc85e7d27a7436fb7e3f3a806a74050613ded933
Chun-wei Fan <fanchunwei@src.gnome.org>
2017-08-15 08:59:44 +0800

    build: Check for sys/time.h
    sys/time.h is not a header that is provided by all compilers that we
    support, so we need to check for it and include it conditionally so that
    things will build normally, especially as Meson builds will build all
    the test programs as well.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=785767

--- tests/pixbuf-randomly-modified.c
+++ tests/pixbuf-randomly-modified.c
@@ -24,7 +24,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+
+#ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
+#endif
+
 #ifdef HAVE_SYS_RESOURCE_H
 #include <sys/resource.h>
 #endif
