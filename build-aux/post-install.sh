d08de097394f327e48544d40a8fb217a0d6424af
Emmanuele Bassi <ebassi@gnome.org>
2018-04-29 18:29:49 +0100

    meson: Add post-install script
    If we're installing gdk-pixbuf ourselves, we should generate the loaders
    cache file — unless DESTDIR is set or we're cross-compiling, in which
    case we assume that there's a packaging system hook in place to do this
    at the end of a full build.

---
+++ build-aux/post-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+libdir="$1"
+binary_version="$2"
+
+if [ -z "$DESTDIR" ]; then
+        mkdir -p "$libdir/gdk-pixbuf-2.0/$binary_version"
+        gdk-pixbuf-query-loaders > "$libdir/gdk-pixbuf-2.0/$binary_version/loaders.cache"
+else
+        echo "***"
+        echo "*** Warning: loaders.cache not built"
+        echo "***"
+        echo "*** You should generate this file manually on the host system"
+        echo "*** using:"
+        echo "***   gdk-pixbuf-query-loaders > $libdir/gdk-pixbuf-2.0/$binary_version/loaders.cache"
+        echo "***"
+fi
