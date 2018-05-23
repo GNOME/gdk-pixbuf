11a1961b57d203a1f9328e96db9c7e0099c0fd77
Emmanuele Bassi <ebassi@gnome.org>
2018-05-23 18:01:26 +0100

    ci: Update the scripts
    Build inside a temporary directory, and ensure that the local
    run-docker.sh script imports the repo inside the Docker container.

--- .gitlab/ci/run-docker.sh
+++ .gitlab/ci/run-docker.sh
@@ -7,5 +7,5 @@ TAG="registry.gitlab.gnome.org/gnome/gdk-pixbuf/master:v1"
 sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
 sudo docker run --rm --security-opt label=disable \
-    --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
+    --volume "$(pwd)/../..:/home/user/app" --workdir "/home/user/app" \
     --tty --interactive "${TAG}" bash
--- .gitlab/ci/test-docker.sh
+++ .gitlab/ci/test-docker.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+builddir=$(mktemp -d build_XXXXXX)
 srcdir=$(pwd)
 
 mkdir -p _ccache
@@ -10,12 +11,18 @@ export CCACHE_DIR="${CCACHE_BASEDIR}/_ccache"
 
 ccache --zero-stats
 ccache --show-stats
+
+# Disable ccache while running Meson, to avoid cached compiler tests
 export CCACHE_DISABLE=true
-meson \
-        _build $srcdir
+meson ${builddir} ${srcdir} || exit $?
 unset CCACHE_DISABLE
 
-cd _build
+cd ${builddir}
 
-ninja
+ninja || exit $?
+
+cd ..
+
+rm -rf ${builddir}
+
 ccache --show-stats
