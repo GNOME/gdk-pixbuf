db43c7d32217f70bc87e9df26c7482ae1d2c78c1
Bastien Nocera <hadess@hadess.net>
2019-03-02 18:13:01 +0100

    gdk-pixbuf: Remove unused GdkPixbufAlphaMode

--- gdk-pixbuf/gdk-pixbuf-core.h
+++ gdk-pixbuf/gdk-pixbuf-core.h
@@ -106,30 +106,6 @@ G_BEGIN_DECLS
 
 
 /**
- * GdkPixbufAlphaMode:
- * @GDK_PIXBUF_ALPHA_BILEVEL: A bilevel clipping mask (black and white)
- *  will be created and used to draw the image.  Pixels below 0.5 opacity
- *  will be considered fully transparent, and all others will be
- *  considered fully opaque.
- * @GDK_PIXBUF_ALPHA_FULL: For now falls back to #GDK_PIXBUF_ALPHA_BILEVEL.
- *  In the future it will do full alpha compositing.
- * 
- * These values can be passed to
- * gdk_pixbuf_xlib_render_to_drawable_alpha() to control how the alpha
- * channel of an image should be handled.  This function can create a
- * bilevel clipping mask (black and white) and use it while painting
- * the image.  In the future, when the X Window System gets an alpha
- * channel extension, it will be possible to do full alpha
- * compositing onto arbitrary drawables.  For now both cases fall
- * back to a bilevel clipping mask.
- */
-typedef enum
-{
-        GDK_PIXBUF_ALPHA_BILEVEL,
-        GDK_PIXBUF_ALPHA_FULL
-} GdkPixbufAlphaMode;
-
-/**
  * GdkColorspace:
  * @GDK_COLORSPACE_RGB: Indicates a red/green/blue additive color space.
  * 
