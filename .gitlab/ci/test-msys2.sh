c763e0a7071f816d7c88410468c88e8511618cec
Emmanuele Bassi <ebassi@gnome.org>
2021-03-31 10:35:36 +0100

    ci: Drop old dependencies from MSYS2 job
     - The Jasper loader has been removed
     - There's no X11 dependency any more
     - We don't use gtk-doc for generating the API reference

--- .gitlab/ci/test-msys2.sh
+++ .gitlab/ci/test-msys2.sh
@@ -14,23 +14,17 @@ pacman --noconfirm -S --needed \
     base-devel \
     mingw-w64-$MSYS2_ARCH-gobject-introspection \
     mingw-w64-$MSYS2_ARCH-glib2 \
-    mingw-w64-$MSYS2_ARCH-jasper \
     mingw-w64-$MSYS2_ARCH-libjpeg-turbo \
     mingw-w64-$MSYS2_ARCH-libpng \
     mingw-w64-$MSYS2_ARCH-libtiff \
     mingw-w64-$MSYS2_ARCH-meson \
-    mingw-w64-$MSYS2_ARCH-toolchain \
-    mingw-w64-$MSYS2_ARCH-gtk-doc
+    mingw-w64-$MSYS2_ARCH-toolchain
 
-meson --buildtype debug \
-    -Dx11=false \
+meson setup --buildtype debug \
     -Dpng=true \
     -Djpeg=true \
     -Dtiff=true \
-    -Djasper=true \
     _build
 
-cd _build
-ninja
-
-meson test
+meson compile -C _build
+meson test -C _build
