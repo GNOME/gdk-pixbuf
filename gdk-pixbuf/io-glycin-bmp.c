9e43f4f8a7cf429f0c53795e1294979cdbea73b1
Matthias Clasen <mclasen@redhat.com>
2024-12-03 13:52:50 -0500

    Make a glycin loader
    This is a very quick attempt to test the feasibility of making
    a loader that wraps glycin, similar to how the native Windows
    loader wraps the Windows apis.
    
    Currently, it only implements incremental loading, ignores
    animations and does not support saving.

---
+++ gdk-pixbuf/io-glycin-bmp.c
@@ -0,0 +1,63 @@
+/* GdkPixbuf library - Glycin image loader
+ *
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Authors: Matthias Clasen <mclasenredhat.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <glib/gi18n-lib.h>
+
+#include "io-glycin-utils.h"
+
+
+#ifndef INCLUDE_glycin
+#define MODULE_ENTRY(function) G_MODULE_EXPORT void function
+#else
+#define MODULE_ENTRY(function) void _gdk_pixbuf__glycin_bmp_ ## function
+#endif
+
+MODULE_ENTRY (fill_vtable) (GdkPixbufModule *module)
+{
+  glycin_fill_vtable (module);
+}
+
+MODULE_ENTRY (fill_info) (GdkPixbufFormat *info)
+{
+  static const GdkPixbufModulePattern signature[] = {
+    { "BM", NULL, 100 },
+    { NULL, NULL, 0 }
+  };
+  static const gchar * mime_types[] = {
+    "image/bmp",
+    "image/x-bmp",
+    "image/x-MS-bmp",
+    NULL
+  };
+  static const gchar * extensions[] = {
+    "bmp",
+    NULL
+  };
+
+  info->name = "bmp";
+  info->signature = (GdkPixbufModulePattern *) signature;
+  info->description = NC_("image format", "BMP");
+  info->mime_types = (gchar **) mime_types;
+  info->extensions = (gchar **) extensions;
+  info->flags = GDK_PIXBUF_FORMAT_THREADSAFE;
+  info->license = "LGPL";
+}
---
+++ gdk-pixbuf/io-glycin-tiff.c
@@ -0,0 +1,64 @@
+/* GdkPixbuf library - Glycin image loader
+ *
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Authors: Matthias Clasen <mclasenredhat.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <glib/gi18n-lib.h>
+
+#include "io-glycin-utils.h"
+
+
+#ifndef INCLUDE_glycin
+#define MODULE_ENTRY(function) G_MODULE_EXPORT void function
+#else
+#define MODULE_ENTRY(function) void _gdk_pixbuf__glycin_tiff_ ## function
+#endif
+
+MODULE_ENTRY (fill_vtable) (GdkPixbufModule *module)
+{
+  glycin_fill_vtable (module);
+}
+
+MODULE_ENTRY (fill_info) (GdkPixbufFormat *info)
+{
+  static const GdkPixbufModulePattern signature[] = {
+    { "MM \x2a", "  z ", 100 },
+    { "II\x2a ", "   z", 100 },
+    { "II* \020   CR\002 ", "   z zzz   z", 0 },
+    { NULL, NULL, 0 }
+  };
+  static const gchar *mime_types[] = {
+    "image/tiff",
+    NULL
+  };
+  static const gchar *extensions[] = {
+    "tiff",
+    "tif",
+    NULL
+  };
+
+  info->name = "tiff";
+  info->signature = (GdkPixbufModulePattern *) signature;
+  info->description = NC_("image format", "TIFF");
+  info->mime_types = (gchar **) mime_types;
+  info->extensions = (gchar **) extensions;
+  info->flags = GDK_PIXBUF_FORMAT_THREADSAFE;
+  info->license = "LGPL";
+}
