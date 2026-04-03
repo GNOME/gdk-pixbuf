GNOME/gdk-pixbuf

commit b08145dcbb84de09ff1c50d3658c739b6924f8fd

io-xpm: Use legacy XPM for gdk_pixbuf_new_from_xpm_data()
The glycin cannot handle the raw data passed to gdk_pixbuf_new_from_xpm_data(),
thus instead of using it use the old code to convert the data into pixbuf.

gdk-pixbuf/gdk-pixbuf-io.c
@@ -2268,7 +2268,7 @@ gdk_pixbuf_new_from_xpm_data (const char **data)
 
         g_return_val_if_fail (data != NULL, NULL);
 
-        xpm_module = _gdk_pixbuf_get_named_module ("xpm", &error);
+        xpm_module = _gdk_pixbuf_get_named_module ("legacy-xpm", &error);
         if (xpm_module == NULL) {
                 g_warning ("Error loading XPM image loader: %s", error->message);
                 g_error_free (error);

gdk-pixbuf/io-xpm.c
@@ -836,11 +836,10 @@ MODULE_ENTRY (fill_info) (GdkPixbufFormat *info)
 		NULL
 	};
 	static const gchar *extensions[] = {
-		"xpm",
 		NULL
 	};
 
-	info->name = "xpm";
+	info->name = "legacy-xpm";
 	info->signature = (GdkPixbufModulePattern *) signature;
 	info->description = NC_("image format", "XPM");
 	info->mime_types = (gchar **) mime_types;

meson.build
@@ -334,6 +334,12 @@ if not others_opt.disabled()
   ]
 endif
 
+if not get_option('legacy_xpm').disabled()
+  enabled_loaders += [
+    'xpm',
+  ]
+endif
+
 # On Windows, check whether we are building the native Windows loaders
 # (it is an "all-or-nothing" option for BMP, EMF, GIF, ICO, JPEG, TIFF and WMF)
 # Note that we currently do not use the native Windows loaders to handle PNG and

commit 62b8f9fd0bb3b862823cd34afce4b389fbd27569

Fix loading of xpm module if built-in
After the xpm module was renamed to legacy-xpm internally, it needs
to be registered under that name in gdk_pixbuf_io_init_builtin().
Otherwise, gdk_pixbuf_new_from_xpm_data() won't find it.

gdk_pixbuf_io_init_builtin(), in turn, needs to match up with
gdk_pixbuf_load_module_unlocked().

Fixes: b08145dc "io-xpm: Use legacy XPM for gdk_pixbuf_new_from_xpm_data()"
Resolves: https://gitlab.gnome.org/GNOME/gdk-pixbuf/-/issues/306
Bug-Debian: https://bugs.debian.org/1132565

gdk-pixbuf/gdk-pixbuf-io.c
@@ -631,7 +631,7 @@ gdk_pixbuf_io_init_builtin (void)
         load_one_builtin_module (tiff);
 #endif
 #ifdef INCLUDE_xpm
-        load_one_builtin_module (xpm);
+        load_one_builtin_module (legacy-xpm);
 #endif
 #ifdef INCLUDE_xbm
         load_one_builtin_module (xbm);
@@ -857,7 +857,7 @@ gdk_pixbuf_load_module_unlocked (GdkPixbufModule *image_module,
         try_module (tiff,tiff);
 #endif
 #ifdef INCLUDE_xpm
-        try_module (xpm,xpm);
+        try_module (legacy-xpm,xpm);
 #endif
 #ifdef INCLUDE_xbm
         try_module (xbm,xbm);